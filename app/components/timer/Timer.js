f30f2f46e8e73875ab5e21fbc24e65cf2910e9d4
MATTHEW MCHUGHES <mmchughes@MTM-OKAL005.local>
2016-09-27 16:35:46 -0500

    added time input component

--- app/components/timer/Timer.js
+++ app/components/timer/Timer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import TaskInputs from '../common/taskInputs';
 import ProgressBar from '../common/progressBar';
+import TimeInput from '../common/timeInput';
 
 export default class Timer extends Component {
   render() {
@@ -9,6 +10,7 @@ export default class Timer extends Component {
       <div>
           <ProgressBar />          
           <TaskInputs />
+          <TimeInput />
           <Link to="/">
             <i className="fa fa-arrow-left fa-3x" />
           </Link>         
